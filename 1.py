7010cb66fee5e515ac6bafff45782936aaf1d527
ZA-IB <jehanzeb7@outlook.com>
2025-01-17 01:56:39 +0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+#Instructions
+#We've provided the lists row_2 and empty_row in the code editor.
+#Use the len() function to find the length of row_2, and store the result in a variable named row_2_length.
+#Use the len() function to find the length of empty_row, and store the result in a variable named empty_row_length.
+
+row_2 = ['Instagram', 0.0, 'USD', 2161558, 4.5]
+empty_row = []
+row_2_length = len(row_2)
+empty_row_length = len(empty_row)
