83ed33a6f4ca058226ef94a76728eb51f179b667
ZA-IB <jehanzeb7@outlook.com>
2025-01-17 02:01:08 +0500

    2.py

---
+++ 2.py
@@ -0,0 +1,15 @@
+#Instructions
+#The lists we've been working with are in the code editor.
+
+#1.Determine the index number of the track_name element 'Instagram' in row_2, and store the answer as an integer in a variable named track_name_index.
+#For example, the currency element 'USD' of row_1 is index number 2, so we'd write currency_index = 2.
+
+#2.Determine the index number of the user_rating element 4.5 in row_3, and store the answer as an integer in a variable named user_rating_index.
+
+row_1 = ['Facebook', 0.0, 'USD', 2974676, 3.5]
+row_2 = ['Instagram', 0.0, 'USD', 2161558, 4.5]
+row_3 = ['Clash of Clans', 0.0, 'USD', 2130805, 4.5]
+
+
+track_name_index = 0 # Remember, the index always starts at 0
+user_rating_index = 4 # Fifth position in the list equals index 4
