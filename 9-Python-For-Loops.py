4cd0164e744f4855bbe4d8d57f4e076a8b1c9b4e
ZA-IB <jehanzeb7@outlook.com>
2025-01-18 03:06:45 +0500

    Create 9-Python-For-Loops.py

---
+++ 9-Python-For-Loops.py
@@ -0,0 +1,10 @@
+#Instructions
+#Use the new technique we've learned to print all app names in the app_names list. We've provided this list in the code editor to the right.
+
+#1-Translate this pattern into Python syntax: for each element in the app_names list, print the app name.
+#-Indent the print() statement by four spaces.
+
+app_names = ['Facebook', 'Instagram', 'Clash of Clans', 'Fruit Ninja Classic', 'Minecraft: Pocket Edition']
+
+for element in app_names:
+    print(element)
